1eeeec93ead8e045321618544f7003ac9d3dbcb7
walkervalentinuss <walkervalentinuss@gmail.com>
2023-10-09 09:17:03 +0700

    First Commit

---
+++ 2122-ge-t04-string-walkervalentinuss/t04_01.c
@@ -0,0 +1,13 @@
+// 12S21012 - Walker Valentinus Simanjuntak
+// 12S21012 - Lamria Magdalena Tampubolon
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int _argv, char **_argc)
+{
+  
+  return 0;
+
+}
---
+++ 2122-ge-t04-string-walkervalentinuss/t04_02.c
@@ -0,0 +1,14 @@
+// 12S21012 - Walker Valentinus Simanjuntak
+// 12S21012 - Lamria Magdalena Tampubolon
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int _argv, char **_argc)
+{
+  
+
+  return 0;
+
+}
